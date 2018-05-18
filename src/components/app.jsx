874ed96013f73c332fff58975a377c89a1b80ac1
mbila <taniambila@yahoo.co.uk>
2018-05-18 18:33:45 +0100

    first commit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const App = props => <h1>{`Hello ${props.name}!`}</h1>;
+import LocationDetails from './location-details';
 
 App.propTypes = {
   name: PropTypes.string,
 };
 
+
+const App = props => (
+  <LocationDetails
+    city={props.location.city}
+    country={props.location.country}
+  />
+);
+
 App.defaultProps = {
   name: 'World',
+);
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
 };
 
-
 export default App;
+
