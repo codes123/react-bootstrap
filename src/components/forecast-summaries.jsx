3bfc3df104ff3674c6a55f03969eaf61dcbc505f
mbila <taniambila@yahoo.co.uk>
2018-05-23 20:59:50 +0100

    step 8 update 2

--- src/components/forecast-summaries.jsx
+++ src/components/forecast-summaries.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ForecastSummary from './forecast-summary';
 
-const ForecastSummaries = props => (
-  <div className="forecast-summaries">
-    {
-     props.Forecast.map(Forecast => (
-       <ForecastSummary
-         key={forecast.date}
-         date={forecast.date}
-         description={forecast.description}
-         icon={forecast.icon}
-         temperature={forecast.temperature.max}
-       />
-    ))
-  }
-  </div>
-);
+const ForecastSummaries = props => <h1 className="forecast-summaries">{props.date}, {props.description}, {props.icon}, {props.temperature}</h1>;    
+     props.Forecast.map(Forecast => ( 
+     ForecastSummary
+         key= forecast.date.Proptypes.string.isRequired
+         date: forecast.date.PropTypes.string.isRequired,
+         description: forecast.description.PropTypes.string.isRequired,
+         icon: forecast.icon.PropTypes.string.isRequired,
+         temperature: forecast.temperature.PropTypes.string.isRequired,
 
 export default ForecastSummaries;
